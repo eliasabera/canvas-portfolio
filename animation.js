f032b646f4009c42542b9ddb2853e2791898ecf7
eliasabera <154503195+eliasabera@users.noreply.github.com>
2025-07-04 07:37:13 +0300

    update

--- animation.js
+++ animation.js
@@ -1,18 +1,18 @@
 //const skills = document.querySelector(".skills");
-const skill = document.querySelectorAll(".skill");
-function callbackFuction(entry) {
-  entry.forEach((e) => {
-    if (e.isIntersecting) {
-      e.target.classList.add("visible");
-    }
-  });
-}
-const options = {
-  threshold: 1,
-};
-let observer = new IntersectionObserver(callbackFuction, options);
+// const skill = document.querySelectorAll(".skill");
+// function callbackFuction(entry) {
+//   entry.forEach((e) => {
+//     if (e.isIntersecting) {
+//       e.target.classList.add("visible");
+//     }
+//   });
+// }
+// const options = {
+//   threshold: 1,
+// };
+// let observer = new IntersectionObserver(callbackFuction, options);
 
-skill.forEach((skill) => {
-  observer.observe(skill)
-})
+// skill.forEach((skill) => {
+//   observer.observe(skill)
+// })
 
