38fd787218efba156d208a41d68a1a77f47e8eff
eliasabera <154503195+eliasabera@users.noreply.github.com>
2025-07-10 11:55:37 +0300

    update

--- animation.js
+++ animation.js
@@ -1,18 +1,35 @@
-// const skills = document.querySelector(".skills");
-// const skill = document.querySelectorAll(".skill");
-// function callbackFuction(entry) {
-//   entry.forEach((e) => {
-//     if (e.isIntersecting) {
-//       e.target.classList.add("visible");
-//     }
-//   });
-// }
-// const options = {
-//   threshold: 1,
-// };
-// let observer = new IntersectionObserver(callbackFuction, options);
+const track = document.getElementById("image-track");
+window.onmousedown = (e) => {
+  track.dataset.mouseDownAt = e.clientX;
+};
+window.onmousemove = (e) => {
+  if (track.dataset.mouseDownAt === "0") return;
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+    maxDelta = window.innerWidth / 2;
 
-// skill.forEach((skill) => {
-//   observer.observe(skill)
-// })
+  const percentage = (mouseDelta / maxDelta) * -100;
+  const rawNext = parseFloat(track.dataset.prevPercentage) + percentage;
+  const nextPercentage = Math.max(Math.min(rawNext, 0), -100);
 
+  track.dataset.percentage = nextPercentage;
+
+  //track.style.transform = `translate(${nextPercentage}%,-50%)`;
+  track.animate(
+    {
+      transform: `translate(${nextPercentage}%,-50%)`,
+    },
+    { duration: 1200, fill: "forwards" }
+  );
+  for (const image of track.getElementsByClassName("image")) {
+    image.animate(
+      {
+        objectPosition: `${nextPercentage + 100}%  50%`,
+      },
+      { duration: 1200, fill: "forwards" }
+    );
+  }
+};
+window.onmouseup = (e) => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage;
+};
